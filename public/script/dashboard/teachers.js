d01203e2154ca606647f15958e4b992891ffc593
Friendly <kirill.tsyganov@internet.ru>
2026-04-26 18:31:12 +0300

    add exit and show window for adding teacher

--- public/script/dashboard/teachers.js
+++ public/script/dashboard/teachers.js
@@ -1,4 +1,10 @@
 const add_btn = document.getElementById("add_btn");
+const dark_phone = document.getElementById("dark_phone");
+const add_teacher_exit_btn = document.getElementById("add_teacher_exit_btn");
+
 add_btn.addEventListener('click',function() {
-    
+    dark_phone.style.visibility = "visible";
+});
+add_teacher_exit_btn.addEventListener("click", function() {
+    dark_phone.style.visibility = "collapse";
 });
