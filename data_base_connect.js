841165d5086db4c8befe46cd7b05b0251641d3f8
Friendly <kirill.tsyganov@internet.ru>
2026-04-26 13:27:22 +0300

    fix bugs in database

--- data_base_connect.js
+++ data_base_connect.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "19614141_Kirill",
-  database: "crm",
+  database: "CheapCRM",
   waitForConnections: true,
   connectionLimit: 10
 });
