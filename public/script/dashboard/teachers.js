80703f32b0c45b48a08dade8180e3ea46e1426ac
Friendly <kirill.tsyganov@internet.ru>
2026-04-29 19:16:19 +0300

    complete frontend part in add teachers form

--- public/script/dashboard/teachers.js
+++ public/script/dashboard/teachers.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", async function () {
     const getTeacher_response = await fetch("/data/getTeacher");
     const teachers = await getTeacher_response.json(); //id avatar name subject company_id
     teachers.data.forEach((element) => {
-        if(element.avatar == null || element.avatar == ""){
-            element.avatar = "/img/user/dashboard/user-solid.png";
-        }
-        teacher_table_tbody.insertAdjacentHTML(
+      if (element.avatar == null || element.avatar == "") {
+        element.avatar = "/img/user/dashboard/user-solid.png";
+      }
+      teacher_table_tbody.insertAdjacentHTML(
         "beforeend",
         `
         <tr>
@@ -28,7 +28,9 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   const add_btn = document.getElementById("add_btn");
   const dark_phone = document.getElementById("dark_phone");
-  const add_teacher_exit_btn = document.getElementById("add_teacher_exit_btn");
+  const add_teacher_exit_btn = document.querySelectorAll(
+    "#add_teacher_exit_btn",
+  );
   let tableHTML = `
     <tr>
      <td><img class="avatar" /></td>
@@ -41,7 +43,40 @@ document.addEventListener("DOMContentLoaded", async function () {
   add_btn.addEventListener("click", function () {
     dark_phone.style.visibility = "visible";
   });
-  add_teacher_exit_btn.addEventListener("click", function () {
-    dark_phone.style.visibility = "collapse";
+  add_teacher_exit_btn.forEach((el) => {
+    el.addEventListener("click", function () {
+      dark_phone.style.visibility = "collapse";
+    });
+  });
+
+  const selector = document.getElementById("countrySelector");
+  const dropdown = document.getElementById("countryList");
+  const selectedCodeDisplay = document.getElementById("selectedCode");
+  const arrow = selector.querySelector(".dropdown-arrow");
+
+  // Открыть/закрыть меню
+  selector.addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdown.classList.toggle("active");
+    // Поворачиваем стрелочку
+    arrow.style.transform = dropdown.classList.contains("active")
+      ? "rotate(180deg)"
+      : "rotate(0deg)";
+  });
+
+  // Выбор страны
+  dropdown.querySelectorAll("li").forEach((item) => {
+    item.addEventListener("click", (e) => {
+      const code = item.getAttribute("data-code");
+      selectedCodeDisplay.textContent = code;
+      dropdown.classList.remove("active");
+      arrow.style.transform = "rotate(0deg)";
+    });
+  });
+
+  // Закрыть, если кликнули мимо
+  document.addEventListener("click", () => {
+    dropdown.classList.remove("active");
+    arrow.style.transform = "rotate(0deg)";
   });
 });
