39c290c060e61c57e72c1cc0f2c64e8fd134dde0
Friendly <kirill.tsyganov@internet.ru>
2026-05-10 11:01:45 +0300

    refactor: install necessary objects

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const routes = require('./routes/routes.js');
+const cors = require('cors');
 
 const app = express();
-console.log("init objects");
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,9 +17,8 @@ app.use(session({
     saveUninitialized: false,
     cookie: { secure: false}
 }));
-console.log("use session");
 app.use('/', routes); 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "frontend", "public")));
 
 app.listen(3000, async () => {
     console.log("your server was be running on http://localhost:3000");
