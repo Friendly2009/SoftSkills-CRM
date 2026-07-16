46cddefcef625249a85d3525563e0199272b88a9
Friendly <kirill.tsyganov@internet.ru>
2026-07-16 14:31:50 +0300

    feat: init the analytic page

---
+++ frontend/src/components/DashboardsComponents/Analytics.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+export const Analytic: React.FC = () => {
+    return(
+        <>
+            <h1>its the annalytics page</h1>
+        </>
+    )
+}
